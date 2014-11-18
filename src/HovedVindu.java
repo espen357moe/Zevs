40b9806b4874efb4f8c7fd818dcda43e95570b22
Espen Moe <espen357moe@gmail.com>
2014-11-18 10:54:54 +0100

    Første commit

---
+++ src/HovedVindu.java
@@ -0,0 +1,14 @@
+import java.awt.GridLayout;
+
+import javax.swing.JFrame;
+
+
+public class HovedVindu extends JFrame {
+	HovedVindu() {
+		setSize(500,500);
+		setTitle("Zevs");
+		setLayout(new GridLayout(0,4));
+		setVisible(true);
+	}
+	
+}
