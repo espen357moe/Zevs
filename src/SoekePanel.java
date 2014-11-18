29df419f206ccfa84c8d13ccde9052358d9c26e0
Espen Moe <espen357moe@gmail.com>
2014-11-18 13:22:37 +0100

    La til system look & feel

--- src/SoekePanel.java
+++ src/SoekePanel.java
@@ -1,5 +1,13 @@
+import java.awt.Container;
+import java.awt.GridLayout;
+
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 public class SoekePanel extends JPanel {
+	SoekePanel() {
+		JTextField soekeFelt = new JTextField();
+		this.add(soekeFelt);
+	}
 
 }
