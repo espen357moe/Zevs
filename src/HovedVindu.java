4910b42d32dc188fc0cdf73f973e6f0e716872af
Espen Moe <espen357moe@gmail.com>
2014-11-18 12:22:29 +0100

    Lagde meny i HovedVindu.java

--- src/HovedVindu.java
+++ src/HovedVindu.java
@@ -1,13 +1,35 @@
+import java.awt.Container;
 import java.awt.GridLayout;
+import java.awt.event.KeyEvent;
 
 import javax.swing.JFrame;
-
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 
 public class HovedVindu extends JFrame {
 	HovedVindu() {
 		setSize(500,500);
 		setTitle("Zevs");
-		setLayout(new GridLayout(0,4));
+		
+		Container hovedContainer = getContentPane();
+		hovedContainer.setLayout(new GridLayout(0,4));
+		
+		JMenuBar menylinje = new JMenuBar();
+        JMenu meny = new JMenu("Valg");
+        JMenuItem m1 = new JMenuItem("Om applikasjonen");
+        JMenuItem m2 = new JMenuItem("Avslutt");
+        
+        meny.setMnemonic(KeyEvent.VK_V);
+        m1.setMnemonic(KeyEvent.VK_O);
+        m2.setMnemonic(KeyEvent.VK_A);
+
+        menylinje.add(meny);
+        meny.add(m1);
+        meny.add(m2);
+
+        setJMenuBar(menylinje);		
+		
 		setVisible(true);
 	}
 	
