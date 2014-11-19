220078edb2427ca9d89685baabdba570b2fad6e4
Jumkah <oysteinpaulsen@hotmail.com>
2014-11-19 11:35:55 +0100

    Test test

--- src/GeografiPanel.java
+++ src/GeografiPanel.java
@@ -1,12 +1,7 @@
-import java.awt.Color;
-
-import javax.swing.JButton;
 import javax.swing.JPanel;
 
 public class GeografiPanel extends JPanel {
-	GeografiPanel() {
-		setBackground(Color.RED);
-		JButton geografiKnapp = new JButton("Her kommer geografidata etterhvert");
-		this.add(geografiKnapp);
-	}
+
 }
+
+public GeografiData hent GeografiData (stedsnavn sn)
