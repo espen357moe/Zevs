3aea8ca04de9e04d75ad0e599225d9e5807fcdf3
Espen Moe <espen357moe@gmail.com>
2014-11-19 11:19:06 +0100

    Fylte ut i placeholder-objektene til GeografiInfo og MeteorologiInfo

--- src/GeografiInfo.java
+++ src/GeografiInfo.java
@@ -1,4 +1,22 @@
 
 public class GeografiInfo {
-
+	public class MeteorologiInfo {
+		//Dette er kun placeholders for infotyper
+		private float meterOverHavet;
+		private String[] koordinater = new String[2];
+		
+		public MeteorologiInfo(float meterOverHavet, String[] koordinater) {
+			this.meterOverHavet = meterOverHavet;
+			this.koordinater = koordinater;
+		}
+		
+		public float meterOverHavet(){
+			return this.meterOverHavet;
+		}
+		
+		public String[] getKoordinater(){
+			return this.koordinater;
+		}	
+	}
+	
 }
