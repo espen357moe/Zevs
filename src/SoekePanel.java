417c55217e4b9eee18fd9b890b1c8f8dff4d0855
Espen Moe <espen357moe@gmail.com>
2014-11-20 11:57:40 +0100

    La til søkeknapp i søkepanelet

--- src/SoekePanel.java
+++ src/SoekePanel.java
@@ -1,6 +1,7 @@
 import java.awt.Container;
 import java.awt.GridLayout;
 
+import javax.swing.JButton;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
@@ -8,6 +9,9 @@ public class SoekePanel extends JPanel {
 	SoekePanel() {
 		JTextField soekeFelt = new JTextField(10);
 		this.add(soekeFelt);
+		
+		JButton soekeKnapp = new JButton("Søk");
+		this.add(soekeKnapp);
 	}
 
 }
