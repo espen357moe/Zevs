baa6b3a752137262c4a5256e6ba57cf3b49efe0b
Espen Moe <espen357moe@gmail.com>
2014-11-20 17:24:49 +0100

    Fjernet noen merge-meldinger i SoekeLogikk

--- src/SoekeLogikk.java
+++ src/SoekeLogikk.java
@@ -19,22 +19,22 @@ public class SoekeLogikk {
 				}
 
 				if (indexfound > -1) {
-<<<<<<< HEAD
+
 
 					System.out.println("Funnet " + " " + line + " ganger" + " på  ");
 					
 					
-//					Omgj�ring til Array testing.  
+//					Omgjøring til Array testing.  
 //			System.out.println (java.util.Arrays.toString(line.split(";")));
 					
-=======
+
 					System.out.println(line + " ganger"
 							+ " på ");
 
 					// Omgjøring til Array testing.
 					// System.out.println
 					// (java.util.Arrays.toString(line.split(" 	")));
->>>>>>> 719a3e864521af4c95f477020b15f3a2ad1a2a96
+
 				}
 				if (indexfound == 0) {
 					System.out.println("Ingen treff");
