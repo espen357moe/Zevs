f2a3c7f2af9401c031143b8dde256e2f45316fc8
Espen Moe <espen357moe@gmail.com>
2014-11-21 14:05:14 +0100

    Endret Look & Feel fra Windows-utseende til native Java-utseende.

--- src/VaerApplikasjon.java
+++ src/VaerApplikasjon.java
@@ -5,22 +5,23 @@ import javax.swing.UnsupportedLookAndFeelException;
 public class VaerApplikasjon {
 
 	public static void main(String[] args) {
-		try {
-            UIManager.setLookAndFeel(
-                    UIManager.getSystemLookAndFeelClassName());
-        }
-        catch (UnsupportedLookAndFeelException e) {
-        	e.printStackTrace();
-        }
-        catch (ClassNotFoundException e) {
-        	e.printStackTrace();
-        }
-        catch (InstantiationException e) {
-        	e.printStackTrace();
-        }
-        catch (IllegalAccessException e) {
-        	e.printStackTrace();
-        }
+
+//		try {
+//            UIManager.setLookAndFeel(
+//                    UIManager.getSystemLookAndFeelClassName());
+//        }
+//        catch (UnsupportedLookAndFeelException e) {
+//        	e.printStackTrace();
+//        }
+//        catch (ClassNotFoundException e) {
+//        	e.printStackTrace();
+//        }
+//        catch (InstantiationException e) {
+//        	e.printStackTrace();
+//        }
+//        catch (IllegalAccessException e) {
+//        	e.printStackTrace();
+//        }
 		
 		new HovedVindu();
 		//new Dagvarsel();
