f0385b68b8d1909f26e630eddc25c4a261ff9772
Lorrides <tarhh@outlook.com>
2014-11-21 20:56:28 +0100

    Gjort Array liste i soekeLogikk komplett

--- src/SoekeLogikk.java
+++ src/SoekeLogikk.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 
 public class SoekeLogikk {
 
+	@SuppressWarnings("unused")
 	public void startSoek(String soekeStreng) {
 		try {
 			BufferedReader bf = new BufferedReader(new FileReader(
@@ -22,13 +23,30 @@ public class SoekeLogikk {
 				if (resultatFunnet > -1) {
 					String strArray[] = resultatAvSok.split("\t");
 					for (int i = 0; i < strArray.length;) {
-						System.out.print("Sted: "+strArray[1] +"    ");
-						System.out.print("Komune: "+strArray[6]+"    ");
-						System.out.println("Fylke: "+strArray[7]);
-						System.out.println("URL: "+strArray[12]+"	");
+						String komunenummer 	= strArray[0];
+						String stedsnavn 		= strArray[1];
+						String prioritet 		= strArray[2];
+						String stedtypeNynorsk 	= strArray[3];
+						String stedtypeBokmål 	= strArray[4];
+						String stedEngelsn 		= strArray[5];
+						String komune 			= strArray[6];
+						String fylke 			= strArray[7];
+						String lengdeGrad 		= strArray[8];
+						String breddeGrad 		= strArray[9];
+						String hoydeOverHavet 	= strArray[10];
+						String urlNyNorsk 		= strArray[11];
+						String urlNorsk 		= strArray[12];
+						String urlEngelsk 		= strArray[13];
+
+						System.out.print("Sted: "+stedsnavn+"    ");
+						System.out.print("Komune: "+komune +"    ");
+						System.out.println("Fylke: "+fylke);
+						System.out.println("URL: " +urlNorsk);
 						System.out.println("------------------------------------------------------------------");
 						System.out.println("");
 						break;
+						
+
 					}
 				}
 			}
