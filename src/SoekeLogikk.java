b20ee463d2d50761c919d3f069610c9500ed4c02
Lorrides <tarhh@outlook.com>
2014-11-24 11:29:33 +0100

    Rettskriving av arrays

--- src/SoekeLogikk.java
+++ src/SoekeLogikk.java
@@ -1,33 +1,33 @@
-
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 
 public class SoekeLogikk {
 
+	@SuppressWarnings("unused")
 	public void startSoek(String soekeStreng) {
 		try {
 			BufferedReader bf = new BufferedReader(new FileReader(
 					"SoekeInfo.txt"));
-			String line;
-			String df = soekeStreng;
-			while ((line = bf.readLine()) != null) {
 
-				int indexfound = line.indexOf(df);
-				if (df.isEmpty()) {
+			String brukerInput = soekeStreng;
+			String resultatAvSoek;
+
+			while ((resultatAvSoek = bf.readLine()) != null) {
+				int resultatFunnet = resultatAvSoek.indexOf(brukerInput);
+				if (brukerInput.isEmpty()) {
 					System.out.println("Ingen søk utført");
 					break;
 				}
 
-<<<<<<< HEAD
 				if (resultatFunnet > -1) {
-					String strArray[] = resultatAvSok.split("\t");
+					String strArray[] = resultatAvSoek.split("\t");
 					for (int i = 0; i < strArray.length;) {
-						String kommunenummer 	= strArray[0];
-						String stedsnavn 		= strArray[1];
+						String kommuneNummer 	= strArray[0];
+						String stedsNavn 		= strArray[1];
 						String prioritet 		= strArray[2];
 						String stedtypeNynorsk 	= strArray[3];
-						String stedtypeBokmaal 	= strArray[4];
+						String stedtypeBokmål 	= strArray[4];
 						String stedEngelsk 		= strArray[5];
 						String kommune 			= strArray[6];
 						String fylke 			= strArray[7];
@@ -38,44 +38,20 @@ public class SoekeLogikk {
 						String urlNorsk 		= strArray[12];
 						String urlEngelsk 		= strArray[13];
 
-						System.out.print("Sted: "+stedsnavn+"    ");
-						System.out.print("Kommune: "+kommune +"    ");
+						System.out.print("Sted: "+stedsNavn+"    ");
+						System.out.print("Komune: "+kommune +"    ");
 						System.out.println("Fylke: "+fylke);
 						System.out.println("URL: " +urlNorsk);
 						System.out.println("------------------------------------------------------------------");
 						System.out.println("");
 						break;
-						
 
 					}
-=======
-				if (indexfound > -1) {
-
-
-					System.out.println("Funnet " + " " + line + " ganger" + " på  ");
-					
-					
-//					Omgjøring til Array testing.  
-//			System.out.println (java.util.Arrays.toString(line.split(";")));
-					
-					System.out.println(line + " ganger"
-							+ " på ");
-					// Omgjøring til Array testing.
-					// System.out.println
-					// (java.util.Arrays.toString(line.split(" 	")));
-
-				}
-				if (indexfound == 0) {
-					System.out.println("Ingen treff");
->>>>>>> 66c572fe8d37d2dcd4b0f3a7ecb8418c70624309
 				}
 			}
 
 			bf.close();
 		} catch (IOException e) {
-			System.out.println("IO Error Occurred: " + e.toString());
-
-		}
-
+			System.out.println("IO Error Occurred: " + e.toString());}
 	}
 }
