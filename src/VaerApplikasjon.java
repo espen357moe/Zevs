9add1e851830a388b582ae2db44fb9267644bade
Espen Moe <espen357moe@gmail.com>
2014-11-24 11:48:15 +0100

    Kommenterte bort SystemLook&Feel, så standard Java-utseendet dukker opp

--- src/VaerApplikasjon.java
+++ src/VaerApplikasjon.java
@@ -6,24 +6,24 @@ import javax.swing.UnsupportedLookAndFeelException;
 public class VaerApplikasjon {
 
 	public static void main(String[] args) {
-		try {
-            // Set System L&F
-            UIManager.setLookAndFeel(
-                    UIManager.getSystemLookAndFeelClassName());
-        }
-        catch (UnsupportedLookAndFeelException e) {
-            // handle exception
-        }
-        catch (ClassNotFoundException e) {
-            // handle exception
-        }
-        catch (InstantiationException e) {
-            // handle exception
-        }
-        catch (IllegalAccessException e) {
-            // handle exception
-        }
-		
+//		try {
+//            // Set System L&F
+//            UIManager.setLookAndFeel(
+//                    UIManager.getSystemLookAndFeelClassName());
+//        }
+//        catch (UnsupportedLookAndFeelException e) {
+//            // handle exception
+//        }
+//        catch (ClassNotFoundException e) {
+//            // handle exception
+//        }
+//        catch (InstantiationException e) {
+//            // handle exception
+//        }
+//        catch (IllegalAccessException e) {
+//            // handle exception
+//        }
+//		
 		new HovedVindu();
 		//new Dagvarsel();
 	}
