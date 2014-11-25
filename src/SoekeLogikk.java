387bdaf17584b0e39a0cfcb56fbccf7e58351f6c
Lorrides <tarhh@outlook.com>
2014-11-25 12:34:04 +0100

    Endret navn på NoregInfo.txt i SoekeLogikk.java

--- src/SoekeLogikk.java
+++ src/SoekeLogikk.java
@@ -10,7 +10,7 @@ public class SoekeLogikk {
 	public void startSoek(String soekeStreng) {
 		try {
 			BufferedReader bf = new BufferedReader(new FileReader(
-					"NeregInfo.txt"));
+					"NoregInfo.txt"));
 
 			String brukerInput = soekeStreng;
 			String resultatAvSoek;
