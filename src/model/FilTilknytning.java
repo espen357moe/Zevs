9b6f48a44c350613bc9738238ea83593c88bea44
Espen Moe <espen357moe@gmail.com>
2014-12-05 22:27:27 +0100

    Lagde skjelett til en klasse som skal ta i mot symbolNummer og spytte ut et bilde

---
+++ src/model/FilTilknytning.java
@@ -0,0 +1,14 @@
+package model;
+
+import java.awt.Image;
+
+public class FilTilknytning {
+	
+	private Image bilde;
+	
+	public Image knyttSymbolnummerTilBilde(int symbolNummer) {
+		if(symbolNummer==0) {
+			return null;
+		}
+	}
+}
