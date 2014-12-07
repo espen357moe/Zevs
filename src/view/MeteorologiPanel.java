916c59bb5dddff8f05e1f0875b8e6b008fb87c3f
Espen Moe <espen357moe@gmail.com>
2014-12-07 10:24:13 +0100

    Ryddet opp i formatering i MeteorologiPanel

--- src/view/MeteorologiPanel.java
+++ src/view/MeteorologiPanel.java
@@ -10,45 +10,42 @@ import model.DataEndret;
 import model.FilTilknytning;
 import model.MeteorologiData;
 
-
 @SuppressWarnings("serial")
-public class MeteorologiPanel extends DataPanel {
-
+public class MeteorologiPanel extends DataPanel {	
 	private final DataEndret endret; 
-	
 	private Image vaerSymbol = null;
-	
 	public DataEndret getEndret() { return endret; }
 	
-	public MeteorologiPanel() {
+	public MeteorologiPanel() {		
 		JLabel vaerSymbolRute = new JLabel();
 		this.setForeground(Color.WHITE);
 		this.add(vaerSymbolRute);
 		vaerSymbolRute.setOpaque(true);
 		vaerSymbolRute.setBackground(Color.DARK_GRAY);
 		lagEtikett("Meteorologiske data");
+		
 		endret = new DataEndret() { @Override
-		public void oppdater(MeteorologiData data) {
-			fjernData();
-			skrivUtData(new Float(data.getNedbor()).toString() + " mm nedbør");
-			skrivUtData(data.getVindBetegnelse());
-			skrivUtData(data.getSkydekke());
-			skrivUtData(new Float(data.getVindHastighet()).toString() + " m/s");
-			skrivUtData(data.getVindRetning());
-			skrivUtData(new Integer(data.getSymbolNummer()).toString());
+			public void oppdater(MeteorologiData data) {
+				fjernData();
+				skrivUtData(new Float(data.getNedbor()).toString() + " mm nedbør");
+				skrivUtData(data.getVindBetegnelse());
+				skrivUtData(data.getSkydekke());
+				skrivUtData(new Float(data.getVindHastighet()).toString() + " m/s");
+				skrivUtData(data.getVindRetning());
+				skrivUtData(new Integer(data.getSymbolNummer()).toString());
+				
+				FilTilknytning ft = new FilTilknytning();
+				vaerSymbol = ft.knyttSymbolNummerTilBilde(data.getSymbolNummer());	
+				tegnVaerSymbol(vaerSymbol);	
+			}
+		
+			public void tegnVaerSymbol(Image vaerSymbol) {
+				ImageIcon icon = new ImageIcon(vaerSymbol);
+				vaerSymbolRute.setIcon(icon);
+			}
 			
-			FilTilknytning ft = new FilTilknytning();
-			vaerSymbol = ft.knyttSymbolnummerTilBilde(data.getSymbolNummer());	
-			tegnVaerSymbol(vaerSymbol);	
-		}
-		
-		public void tegnVaerSymbol(Image vaerSymbol) {
-			ImageIcon icon = new ImageIcon(vaerSymbol);
-			vaerSymbolRute.setIcon(icon);
-		}
-		
 		};
 		
-		
 	}
+	
 }
