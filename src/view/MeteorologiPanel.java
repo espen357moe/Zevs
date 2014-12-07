c4057dd04b4c557033d6082a684e5c19e2ea3051
Espen Moe <espen357moe@gmail.com>
2014-12-07 13:31:31 +0100

    Kommenterte ut linjene som tegner værsymbolet

--- src/view/MeteorologiPanel.java
+++ src/view/MeteorologiPanel.java
@@ -13,15 +13,14 @@ import model.MeteorologiData;
 @SuppressWarnings("serial")
 public class MeteorologiPanel extends DataPanel {	
 	private final DataEndret endret; 
-	private Image vaerSymbol = null;
+	//private Image vaerSymbol = null;
 	public DataEndret getEndret() { return endret; }
 	
 	public MeteorologiPanel() {		
-		JLabel vaerSymbolRute = new JLabel();
-		this.setForeground(Color.WHITE);
-		this.add(vaerSymbolRute);
-		vaerSymbolRute.setOpaque(true);
-		vaerSymbolRute.setBackground(Color.DARK_GRAY);
+//		JLabel vaerSymbolRute = new JLabel();
+//		this.add(vaerSymbolRute);
+//		vaerSymbolRute.setOpaque(true);
+//		vaerSymbolRute.setBackground(Color.DARK_GRAY);
 		lagEtikett("Meteorologiske data");
 		
 		endret = new DataEndret() { @Override
@@ -34,15 +33,15 @@ public class MeteorologiPanel extends DataPanel {
 				skrivUtData(data.getVindRetning());
 				skrivUtData(new Integer(data.getSymbolNummer()).toString());
 				
-				FilTilknytning ft = new FilTilknytning();
-				vaerSymbol = ft.knyttSymbolNummerTilBilde(data.getSymbolNummer());	
-				tegnVaerSymbol(vaerSymbol);	
+//				FilTilknytning ft = new FilTilknytning();
+//				vaerSymbol = ft.knyttSymbolNummerTilBilde(data.getSymbolNummer());	
+//				tegnVaerSymbol(vaerSymbol);	
 			}
 		
-			public void tegnVaerSymbol(Image vaerSymbol) {
-				ImageIcon icon = new ImageIcon(vaerSymbol);
-				vaerSymbolRute.setIcon(icon);
-			}
+//			public void tegnVaerSymbol(Image vaerSymbol) {
+//				ImageIcon icon = new ImageIcon(vaerSymbol);
+//				vaerSymbolRute.setIcon(icon);
+//			}
 			
 		};
 		
