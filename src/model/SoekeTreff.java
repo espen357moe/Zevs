cdf5bca10aebb14f1d9e8ae0f8102a5fd544cc79
Lorrides <tarhh@outlook.com>
2014-12-10 23:45:54 +0100

    Ryddet opp i diverse klasser, samt gjort om syso setninger til feilDialog

--- src/model/SoekeTreff.java
+++ src/model/SoekeTreff.java
@@ -1,31 +1,33 @@
 package model;
+
 public class SoekeTreff {
-	
+
 	private String fylke;
 	private String kommune;
 	private String stedsNavn;
 	private String urlNorsk;
-	
-	public SoekeTreff(String fylke, String kommune, String stedsNavn, String urlNorsk){
+
+	public SoekeTreff(String fylke, String kommune, String stedsNavn,
+			String urlNorsk) {
 		this.fylke = fylke;
 		this.kommune = kommune;
 		this.stedsNavn = stedsNavn;
 		this.urlNorsk = urlNorsk;
 	}
-	
-	public String getFylke(){
+
+	public String getFylke() {
 		return this.fylke;
 	}
-	
-	public String getKommune(){
+
+	public String getKommune() {
 		return this.kommune;
 	}
-	
-	public String getStedsNavn(){
+
+	public String getStedsNavn() {
 		return this.stedsNavn;
 	}
-	
-	public String getUrlNorsk(){
+
+	public String getUrlNorsk() {
 		return this.urlNorsk;
 	}
 }
--- src/view/HovedVindu.java
+++ src/view/HovedVindu.java
@@ -1,4 +1,5 @@
 package view;
+
 import java.awt.Container;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
@@ -11,81 +12,74 @@ import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 
-
-@SuppressWarnings("serial")
 public class HovedVindu extends JFrame implements ActionListener {
-	
+
 	public HovedVindu() {
 
-		setSize(420,600);
-		setTitle("Zevs");	
+		setSize(420, 600);
+		setTitle("Zevs");
 		setIconImage(new ImageIcon("Zevs40.png").getImage());
 		setResizable(false);
 		setLocationByPlatform(true);
-		
+
 		Container hovedContainer = getContentPane();
-		hovedContainer.setLayout(new GridLayout(4,0));
-		
+		hovedContainer.setLayout(new GridLayout(4, 0));
+
 		JMenuBar menylinje = new JMenuBar();
-        JMenu meny = new JMenu("Valg");
-        JMenuItem m1 = new JMenuItem("Om applikasjonen");
-        JMenuItem m2 = new JMenuItem("Avslutt");
-        
-        meny.setMnemonic(KeyEvent.VK_V);
-        m1.setMnemonic(KeyEvent.VK_O);
-        m2.setMnemonic(KeyEvent.VK_A);
-        
-        m1.addActionListener(this);
-        
-        menylinje.add(meny);
-        meny.add(m1);
-        meny.add(m2);
-              
-        m1.addActionListener(new ActionListener(){
-        	
-        	public void actionPerformed(ActionEvent ev) {
-        		new OmVindu();
-        }
-        });
-        
-        m2.addActionListener(new ActionListener() {
-        	
-            public void actionPerformed(ActionEvent ev) {
-            	System.exit(0);
-            }
-        }); 
-        
-        setJMenuBar(menylinje);	
-                
-        SoekePanel soekePanel = new SoekePanel();
-        hovedContainer.add(soekePanel);
-        
-        GeografiPanel geografiPanel = new GeografiPanel();
-        hovedContainer.add(geografiPanel);
-        
-        MeteorologiPanel meteorologiPanel = new MeteorologiPanel();
-        hovedContainer.add(meteorologiPanel);
-        
-//        BonusPanel bonusPanel = new BonusPanel();
-//        hovedContainer.add(bonusPanel); 
-        
-        VaerVarselPanel vaerVarselPanel = new VaerVarselPanel();
-        hovedContainer.add(vaerVarselPanel);
-        
-        soekePanel.addAbonnent(meteorologiPanel.getEndret());
-        soekePanel.addAbonnent(geografiPanel.getEndret());
-//		soekePanel.addAbonnent(bonusPanel.getEndret());
-        soekePanel.addAbonnent(vaerVarselPanel.getEndret());
-        
-        meteorologiPanel.getEndret();
-        		
+		JMenu meny = new JMenu("Valg");
+		JMenuItem m1 = new JMenuItem("Om applikasjonen");
+		JMenuItem m2 = new JMenuItem("Avslutt");
+
+		meny.setMnemonic(KeyEvent.VK_V);
+		m1.setMnemonic(KeyEvent.VK_O);
+		m2.setMnemonic(KeyEvent.VK_A);
+
+		m1.addActionListener(this);
+
+		menylinje.add(meny);
+		meny.add(m1);
+		meny.add(m2);
+
+		m1.addActionListener(new ActionListener() {
+
+			public void actionPerformed(ActionEvent ev) {
+				new OmVindu();
+			}
+		});
+
+		m2.addActionListener(new ActionListener() {
+
+			public void actionPerformed(ActionEvent ev) {
+				System.exit(0);
+			}
+		});
+
+		setJMenuBar(menylinje);
+
+		SoekePanel soekePanel = new SoekePanel();
+		hovedContainer.add(soekePanel);
+
+		GeografiPanel geografiPanel = new GeografiPanel();
+		hovedContainer.add(geografiPanel);
+
+		MeteorologiPanel meteorologiPanel = new MeteorologiPanel();
+		hovedContainer.add(meteorologiPanel);
+
+		VaerVarselPanel vaerVarselPanel = new VaerVarselPanel();
+		hovedContainer.add(vaerVarselPanel);
+
+		soekePanel.addAbonnent(meteorologiPanel.getEndret());
+		soekePanel.addAbonnent(geografiPanel.getEndret());
+		soekePanel.addAbonnent(vaerVarselPanel.getEndret());
+
+		meteorologiPanel.getEndret();
+
 		setVisible(true);
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		// TODO Auto-generated method stub
-		
+
 	}
-	
+
 }
